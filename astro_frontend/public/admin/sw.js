418e443eab7ff9d6983ec6adf621ccfa3064f298
tumbarell <a.irsula.t@gmail.com>
2024-10-08 23:15:45 -0400

    agregando especificaciones de configuracion para pwa

---
+++ astro_frontend/public/admin/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'admin-v1';
+const urlsToCache = [
+  '/admin/',
+  '/admin/index.html',
+  '/admin/styles/main.css',
+  '/admin/script/main.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.includes('/ventas-finalizadas')) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        })
+    );
+  }
+});
---
+++ astro_frontend/public/ofertas/sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'ofertas-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles/main.css',
+  '/script/main.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (!event.request.url.includes('/')) {
+    event.respondWith(
+      caches.match(event.request)
+        .then((response) => {
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        })
+    );
+  }
+});
